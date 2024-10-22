64f3b974616c26797ffa2256886cb995ba49c01b
Dalsukh Tapaniya <dalsukhtapaniya@gmail.com>
2024-10-22 10:45:38 +0530

    config error fix

--- vue.config.mjs
+++ vue.config.mjs
@@ -2,4 +2,8 @@ import { defineConfig } from '@vue/cli-service';
 
 export default defineConfig({
   transpileDependencies: true,
+  publicPath: './',  // Change this from '/' to './'
+  configureWebpack: {
+    devtool: 'source-map'
+  }
 });
